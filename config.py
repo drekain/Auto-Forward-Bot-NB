e9a091c861aba4ec1f1862393bfab1d262b4f988
drekain <drekainsmith@gmail.com>
2026-01-28 10:23:55 +0530

    Change DB_NAME environment variable default value

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ API_HASH = os.environ.get("API_HASH", "")
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
 
 MONGO_URI = os.environ.get("MONGO_URI", "")
-DB_NAME = os.environ.get("DB_NAME", "SilentXBotz")
+DB_NAME = os.environ.get("DB_NAME", "forwardXBot")
 
 WEB_SERVER = os.environ.get("WEB_SERVER", "True").lower() in ("true", "1", "t")
 PORT = int(os.environ.get("PORT", "8080"))
